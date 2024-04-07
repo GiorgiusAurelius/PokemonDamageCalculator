7ca962b41ef2ee9eafc1f7f680ea2994afc7f09e
DiegoMartin-prog <diego.martin@mail.polimi.it>
2024-04-07 20:20:52 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def main():
+    """
+    This function will start the program, start the CLI and ask the player for the Pokemons to visualize.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
